ff2f24af615ea179e38555be1b95fd988e815fce
devanshpandit <devansh44@gmail.com>
2018-08-02 22:19:26 -0700

    adding numpy tutorial

---
+++ numpy.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  2 21:51:56 2018
+
+@author: Devansh
+"""
+
+import numpy as np 
+a = np.array([1,2,3]) 
+print (a)
+
+
+
+print("x"*50)
+
+
+
+import numpy as np 
+a = np.array([[1, 2], [3, 4]]) 
+print (a)
+
+
+
+
+
+print("x"*50)
+
+
+# minimum dimensions 
+import numpy as np 
+a = np.array([1, 2, 3,4,5], ndmin = 2) 
+print (a)
+
+
+
+
+print("x"*50)
+
+
+
+
+
+print("x"*50)
+
+
+
+
+
+
+print("x"*50)
+
+
+
+
+print("x"*50)
+
+
+
+
+print("x"*50)
+
+
+
+
+
+print("x"*50)
+
+
+
+
+
+
+print("x"*50)
+
+
+
+
+
+print("x"*50)
+
+
+
+
+
+print("x"*50)
+
+
+
+
+
+
+print("x"*50)
+
+
+
+
+print("x"*50)
+
+
+
+
+print("x"*50)
+
+
+
+
+
+print("x"*50)
